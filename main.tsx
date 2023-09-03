21a9978dc15ab0301ee23d73b24dba41ce89024c
manman <108946238+mnuel1@users.noreply.github.com>
2023-09-04 03:04:17 +0800

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './components/pages/App.tsx'
+import App from './src/components/pages/App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
